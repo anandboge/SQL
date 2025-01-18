96e24a31cb5eb2b54a2b2f32ca85a97acf7a3fe6
Boge Ananda Vardhan <157228272+anandboge@users.noreply.github.com>
2025-01-18 20:05:19 +0530

    5. LIKE operator.sql

--- 2. Retrieving data from the single table/5. LIKE operator.sql
+++ 2. Retrieving data from the single table/5. LIKE operator.sql
@@ -1,6 +1,8 @@
 SELECT *
 FROM customers
+  
 -- WHERE last_name LIKE '%y' -- % means 0 or more
 -- WHERE last_name LIKE '%y%'
 -- WHERE first_name LIKE '___a%' -- _ no of chars
+  
 WHERE city LIKE '%t__'
