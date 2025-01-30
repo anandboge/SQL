be41c67a62dc55b4efbacf892c97bdb2f2f84792
Boge Ananda Vardhan <157228272+anandboge@users.noreply.github.com>
2025-01-30 21:48:10 +0530

    3.1 Inserting Multiple Rows Ex.sql

--- 4. Inserting, Updating and Deleting Data/3.1 Inserting Multiple Rows Ex.sql
+++ 4. Inserting, Updating and Deleting Data/3.1 Inserting Multiple Rows Ex.sql
@@ -3,4 +3,4 @@ USE sql_store;
 INSERT INTO products (product_id, name, quantity_in_stock, unit_price)
 VALUES(11,'Boat Watch', 10, 4.69),
 	  (12, 'Llyod AC', 13, 16.29),
-	  (13, 'IQOO Z6', 5, 22.99)
+	  (13, 'IQOO Z6', 5, 22.99) 
