b87dc9642a5ec3509b31a0be9692101e30de8672
Boge Ananda Vardhan <157228272+anandboge@users.noreply.github.com>
2025-02-02 22:11:50 +0530

    5. Creating a copy of a table.sql

--- 4. Inserting, Updating and Deleting Data/5. Creating a copy of a table.sql
+++ 4. Inserting, Updating and Deleting Data/5. Creating a copy of a table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE orders_archived AS
-SELECT * FROM orders
+SELECT * FROM orders 
 
 -- After execution orders data will be copied to orders_archived and check once for your confirmation, 
 -- then right click on orders_archived on schemas and truncate table.
